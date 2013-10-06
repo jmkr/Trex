99bc6c27e5f495e9d989efbc176d809ebc69f149
John Meeker <jmeeker516@gmail.com>
2013-10-05 22:41:29 -0700

    Inital project and imagepicker started

---
+++ Bex/JKMTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JKMTableViewController.h
+//  Bex
+//
+//  Created by John Meeker on 10/5/13.
+//  Copyright (c) 2013 John Meeker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKMTableViewController : UIViewController
+
+@end
---
+++ Bex/JMKTableViewController.m
@@ -0,0 +1,54 @@
+//
+//  JKMTableViewController.m
+//  Bex
+//
+//  Created by John Meeker on 10/5/13.
+//  Copyright (c) 2013 John Meeker. All rights reserved.
+//
+
+#import "JKMTableViewController.h"
+
+@interface JKMTableViewController ()
+
+@end
+
+@implementation JKMTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    // Reshow the status bar when returning from the UIImagePickerController.
+    ([[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone]);
+}
+
+- (IBAction) useCamera: (id)sender
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.allowsEditing = YES;
+    if (([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]))
+    {
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:picker animated:YES completion:nil];
+        
+        // Hide the status bar.
+        ([[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade]);
+    }
+    else
+    {
+        // Alert: No camera found
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
